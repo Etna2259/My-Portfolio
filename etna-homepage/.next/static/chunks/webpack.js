9bd6ba3e233e94d32d6a640923fdf5bea1ac6c2e
canconan123 <thangqun@gmail.com>
2022-06-26 02:55:14 +0900

    Ignore server files

--- etna-homepage/.next/static/chunks/webpack.js
+++ etna-homepage/.next/static/chunks/webpack.js
@@ -131,7 +131,7 @@
 /******/ 	
 /******/ 	/* webpack/runtime/getFullHash */
 /******/ 	!function() {
-/******/ 		__webpack_require__.h = function() { return "1e93f78de3d401c3"; }
+/******/ 		__webpack_require__.h = function() { return "c93e415f3fd81eda"; }
 /******/ 	}();
 /******/ 	
 /******/ 	/* webpack/runtime/global */
