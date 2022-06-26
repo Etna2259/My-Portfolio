8e0528528fb4aa5b029a6f0fd9134951839b18e3
canconan123 <thangqun@gmail.com>
2022-06-26 23:51:23 +0900

    Added Paragraph, bio, section module

--- etna-homepage/pages/_app.js
+++ etna-homepage/pages/_app.js
@@ -3,15 +3,15 @@ import Layout from '../components/layouts/main'
 import Fonts from '../components/fonts'
 import theme from '../lib/theme'
 
-const Website = ({ Component, pageProps, router}) => {
-    return (
-        <ChakraProvider theme={theme}>
-            <Fonts />
-            <Layout router={router}>
-                <Component {...pageProps} key={router.route} />
-            </Layout>
-        </ChakraProvider>
-    )
+const Website = ({ Component, pageProps, router }) => {
+  return (
+    <ChakraProvider theme={theme}>
+      <Fonts />
+      <Layout router={router}>
+        <Component {...pageProps} key={router.route} />
+      </Layout>
+    </ChakraProvider>
+  )
 }
 
 export default Website
